6f4983e4fb85bb5cfdf7efefe41a54a0fe7b430d
avinashbharti97 <avinashbharti97@gmail.com>
2020-10-18 12:23:29 +0530

    fetch api with card view

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Card} from 'react-native-elements';
 
 export default function App() {
+
+  const [state, setState] = useState([])
+  useEffect(()=> {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json =>setState(json))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={ styles.container}>
+      <View >
+        <ScrollView>
+          {state.map( d=> 
+            <Card title="Profile">
+              <Text style={styles.paragraph}>
+                  {d.title}
+              </Text>
+            </Card>
+          )}
+        </ScrollView>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+    </>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e'
+  }
 });
